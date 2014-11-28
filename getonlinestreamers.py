028b317612f2a8e92335b96a5c2112f18c831a90
Invisible <yeahunter@gmail.com>
2014-11-29 00:41:47 +0100

    Streamerek csekkolasa db-bol (fajlok helyett) Signed-off-by: Invisible <yeahunter@gmail.com>

--- getonlinestreamers.py
+++ getonlinestreamers.py
@@ -1,28 +1,50 @@
 import urllib, urllib2, json, os
-push_url = "http://push.peti.ml/pub?id=streams"
-directory = os.path.dirname(os.path.realpath(__file__))
+import MySQLdb
 
-url = "https://api.twitch.tv/kraken/streams?limit=100&channel=" + open(directory + '/foll').readlines()[0]
+class Streamer(object):
+    def __init__(self, Name=None, Online=0):
+        self.Name = Name
+        self.Online = Online
+
+db = MySQLdb.connect("localhost","MySQLuser","MySQLpassword","MySQLdatabase" )
+cursor = db.cursor()
+cursor.execute("SELECT `Name`, `Online` FROM `Channels`")
+streamerek = []
+for (Name, Online) in cursor:
+    streamerek.append(Streamer(Name, Online))
+
+
+streamerekapihoz = ""
+for Streamer in streamerek:
+    streamerekapihoz = streamerekapihoz + ',%s' % Streamer.Name
+
+akikstreamelnek = ""
+
+
+push_url = "http://push.peti.ml/pub?id=streams"
+
+url = "https://api.twitch.tv/kraken/streams?limit=100&channel=" + streamerekapihoz[1:]
 response = urllib.urlopen(url);
 data = json.loads(response.read())
 
-if not os.path.exists(directory + '/online1'):
-    open(directory + '/online1', 'w').close() 
-
-f = open(directory + '/_online1','w')
 for key, value in data.items():
     if key in 'streams':
         for keyke, valueka in enumerate(value):
             streamername = valueka["channel"]["display_name"]
-            if streamername not in open(directory + '/online1').read():
-                f.write(streamername + "\n")
-                # print streamername + " nincs fajlban"
-                req = urllib2.Request(push_url, "TwitchTV | " + streamername + " just went live!")
-                rsp = urllib2.urlopen(req)
-            else:
-                f.write(streamername + "\n")
-                # print streamername + " faljban"
+            cursor.execute("UPDATE `Channels` SET `Online` = 1 WHERE `Name` = '%s' AND `Online` = 0" % streamername)
+            cursor.fetchone()
+            db.commit()
 
-f.close()
-os.remove(directory + '/online1')
-os.rename(directory + '/_online1', directory + '/online1')
+            if cursor.rowcount == 1:
+                req = urllib2.Request(push_url, "TESZT! - TwitchTV | " + streamername + " just went live!")
+                rsp = urllib2.urlopen(req)
+
+            akikstreamelnek = akikstreamelnek + ',\'%s\'' % streamername
+
+
+akikstreamelnek = "(" + akikstreamelnek[1:] + ")"
+cursor.execute("UPDATE `Channels` Set `Online` = 0 WHERE `Name` NOT IN %s AND `Online` = 1" % akikstreamelnek)
+cursor.fetchone()
+db.commit()
+
+db.close()
--- sql/base.sql
+++ sql/base.sql
@@ -1,10 +1,14 @@
 CREATE TABLE `Channels` (
-	`ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`Name` VARCHAR(75) NOT NULL COMMENT 'TwitchTV csatornanev',
-	`Inserted` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Ekkor kerult be a tablaba',
+	`ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`Name` VARCHAR(75) NOT NULL COMMENT 'TwitchTV csatornanev' COLLATE 'utf8_unicode_ci',
+	`Online` TINYINT(2) NOT NULL DEFAULT '0' COMMENT 'Status: 0 - Offline, 1 - Online',
+	`Inserted` DATETIME NOT NULL COMMENT 'Ekkor kerult be a tablaba',
+	`Updated` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Ekkor frissult a tablaban, ha Online',
 	PRIMARY KEY (`ID`),
 	UNIQUE INDEX `Name` (`Name`)
 )
 COMMENT='TwitchTV csatornai, akiket az OSN felhasznalok kovetnek.'
 COLLATE='utf8_unicode_ci'
-ENGINE=InnoDB;
+ENGINE=InnoDB
+;
+
