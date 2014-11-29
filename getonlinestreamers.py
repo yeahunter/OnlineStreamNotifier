0a3a24b46bbfb57b278227ec865a5a24d6511008
Invisible <yeahunter@gmail.com>
2014-11-29 19:38:16 +0100

    tabla szerkezet frissitve/pontositva Signed-off-by: Invisible <yeahunter@gmail.com>

--- getonlinestreamers.py
+++ getonlinestreamers.py
@@ -2,16 +2,16 @@ import urllib, urllib2, json, os
 import MySQLdb
 
 class Streamer(object):
-    def __init__(self, Name=None, Online=0):
+    def __init__(self, Name=None, Status=0):
         self.Name = Name
-        self.Online = Online
+        self.Status = Status
 
 db = MySQLdb.connect("localhost","MySQLuser","MySQLpassword","MySQLdatabase" )
 cursor = db.cursor()
-cursor.execute("SELECT `Name`, `Online` FROM `Channels`")
+cursor.execute("SELECT `Name`, `Status` FROM `Channels`")
 streamerek = []
-for (Name, Online) in cursor:
-    streamerek.append(Streamer(Name, Online))
+for (Name, Status) in cursor:
+    streamerek.append(Streamer(Name, Status))
 
 
 streamerekapihoz = ""
@@ -31,19 +31,19 @@ for key, value in data.items():
     if key in 'streams':
         for keyke, valueka in enumerate(value):
             streamername = valueka["channel"]["display_name"]
-            cursor.execute("UPDATE `Channels` SET `Online` = 1 WHERE `Name` = '%s' AND `Online` = 0" % streamername)
+            cursor.execute("UPDATE `Channels` SET `Status` = 1, `Updated` = NOW() WHERE `Name` = '%s' AND `Status` = 0" % streamername)
             cursor.fetchone()
             db.commit()
 
             if cursor.rowcount == 1:
-                req = urllib2.Request(push_url, "TESZT! - TwitchTV | " + streamername + " just went live!")
+                req = urllib2.Request(push_url, "TwitchTV | " + streamername + " just went live!")
                 rsp = urllib2.urlopen(req)
 
             akikstreamelnek = akikstreamelnek + ',\'%s\'' % streamername
 
 
 akikstreamelnek = "(" + akikstreamelnek[1:] + ")"
-cursor.execute("UPDATE `Channels` Set `Online` = 0 WHERE `Name` NOT IN %s AND `Online` = 1" % akikstreamelnek)
+cursor.execute("UPDATE `Channels` Set `Status` = 0, `Updated` = NOW() WHERE `Name` NOT IN %s AND `Status` = 1" % akikstreamelnek)
 cursor.fetchone()
 db.commit()
 
--- sql/base.sql
+++ sql/base.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `Channels` (
 	`ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`Name` VARCHAR(75) NOT NULL COMMENT 'TwitchTV csatornanev' COLLATE 'utf8_unicode_ci',
-	`Online` TINYINT(2) NOT NULL DEFAULT '0' COMMENT 'Status: 0 - Offline, 1 - Online',
-	`Inserted` DATETIME NOT NULL COMMENT 'Ekkor kerult be a tablaba',
-	`Updated` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Ekkor frissult a tablaban, ha Online',
+	`Status` TINYINT(2) NOT NULL DEFAULT '0' COMMENT '0 - Offline, 1 - Online',
+	`Inserted` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Ekkor kerult be a tablaba',
+	`Updated` DATETIME NULL DEFAULT NULL COMMENT 'Ha a Status 1, akkor a stream kezdetet mutatja, ha a Status 0, akkor ert veget a stream',
 	PRIMARY KEY (`ID`),
 	UNIQUE INDEX `Name` (`Name`)
 )
@@ -11,4 +11,3 @@ COMMENT='TwitchTV csatornai, akiket az OSN felhasznalok kovetnek.'
 COLLATE='utf8_unicode_ci'
 ENGINE=InnoDB
 ;
-
